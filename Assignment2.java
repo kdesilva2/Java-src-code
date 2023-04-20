fedb83aae74bea77295eea859ea6b40fcf3e6beb
Kianna D <65618355+kdesilva2@users.noreply.github.com>
2023-04-20 14:58:28 -0400

    Add files via upload

---
+++ Assignment2.java
@@ -0,0 +1,38 @@
+public class Assignment2 {
+    
+public static void main(String[] args) {
+    
+//#2 Name and Initials
+
+
+//1. String Variables with the names
+
+ String firstName = "Kianna";
+
+ String middleName = "Elle";
+
+ String lastName ="Desilva";
+
+
+//2. Use charAt so the code can grab the initials from the strings
+
+ char firstInitial = firstName.charAt(0);
+
+ char middleInitial = middleName.charAt(0);
+
+ char lastInitial = lastName.charAt(0);
+
+
+//3. System print out Names and Initials 
+
+ System.out.println("Full Name: " + firstName + " " + middleName + " " +
+ lastName );
+
+ System.out.println("Initials: " + firstInitial + ". " + middleInitial
+ + ". " + lastInitial);
+
+ 
+
+}
+
+}
